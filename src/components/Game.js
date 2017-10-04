79a0286fc11ebb4bb3c0bf4f8f5a7e440c62c7b5
d-kang <david.kang714@gmail.com>
2017-10-04 08:31:51 -0700

    Add in Game Components With Basic Styling

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,14 +5,24 @@ class Game extends PureComponent {
   render() {
     return (
       <div>
-        <div >42</div>
+        <div style={styles.target}>42</div>
         <div>
-          This is game
+          <div>6</div>
+          <div>8</div>
+          <div>40</div>
+          <div>33</div>
+          <div>66</div>
+          <div>3</div>
         </div>
       </div>
     );
   }
-
 }
+
+const styles = {
+  target: {
+    backgroundColor: '#888',
+  },
+};
 
 export default Game;
