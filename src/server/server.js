e95f45f36709a8dab75980d98639feb0db51de76
d-kang <david.kang714@gmail.com>
2017-10-05 06:55:20 -0700

    Init Cors Express

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 app.use(express.static(process.env.PWD + '/public'));
 
 app.listen(5000, () => {
-  console.log('server is running on http://localhost:5000')
-})
+  console.log('server is running on http://localhost:5000');
+});
