c058ace8180411558816e97f53f74c7f6ff8bac1
d-kang <david.kang714@gmail.com>
2017-10-05 08:03:29 -0700

    Save Before Redux

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,17 +1,21 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import shuffle from 'lodash.shuffle';
+
 import PlayNumber from './PlayNumber';
-class Game extends PureComponent {
-  // after 10 seconds, game over
+
+class Game extends React.PureComponent {
   state = {
     selectedIds: [],
     remainingSeconds: 10,
-  }
+  };
+
+  gameStatus = 'PLAYING';
 
   componentDidMount() {
     this.intervalId = setInterval(() => {
       this.setState((prevState) => {
         if (prevState.remainingSeconds === 0) {
-          clearInterval(intervalId);
+          clearInterval(this.intervalId);
           return null;
         }
         return { remainingSeconds: prevState.remainingSeconds - 1 };
@@ -19,67 +23,77 @@ class Game extends PureComponent {
     }, 1000);
   }
 
-  // gameStatus = 'PLAYING'
-
-  // gameState: PLAYING, WON, LOST
-
-  gameStatus = () => {
-    const selectedSum = this.state.selectedIds.reduce(
+  calcGameStatus = (nextState) => {
+    const selectedSum = nextState.selectedIds.reduce(
       (acc, curr) => acc + this.playNumbers[curr],
       0
     );
-    if (this.state.remainingSeconds === 0) {
-      return 'LOST';
-    } else if (selectedSum < this.target) {
-      return 'PLAYING';
-    } else if (selectedSum === this.target) {
-      return 'WON';
-    } else if (selectedSum === this.target) {
+    if (nextState.remainingSeconds === 0 || selectedSum > this.target) {
       return 'LOST';
     }
-
-  }
-
+    if (selectedSum < this.target) {
+      return 'PLAYING';
+    }
+    if (selectedSum === this.target) {
+      return 'WON';
+    }
+  };
 
   selectId = (id) => {
     this.setState((prevState) => {
       return { selectedIds: [...prevState.selectedIds, id] };
     });
-  }
+  };
 
-  // target = 10 + Math.floor(50 * Math.random());
-  // playNumbers = [1, 4, 5, 2, 6, 10];
-  playNumbers = Array.from({ length: 6 }).map(() =>
-    2 + Math.floor(12 * Math.random())
+  playNumbers = Array.from({ length: 6 }).map(
+    () => 2 + Math.floor(12 * Math.random())
   );
-  target = this.playNumbers.slice(0, 3).reduce((acc, curr) => acc + curr, 0);
 
-  // TODO: SHUFFLE playNumbers
+  target = this.playNumbers.slice(0, 4).reduce((acc, curr) => acc + curr, 0);
+
+  playNumbers = shuffle(this.playNumbers);
 
   componentWillUpdate(nextProps, nextState) {
-    if (this.gameStatus() !== 'PLAYING') {
+    if (
+      nextState.selectedIds !== this.state.selectedIds ||
+      nextState.remainingSeconds === 0
+    ) {
+      this.gameStatus = this.calcGameStatus(nextState);
+    }
+
+    if (this.gameStatus !== 'PLAYING') {
       clearInterval(this.intervalId);
     }
   }
+
   render() {
-    const gameStatus = this.gameStatus();
+    const gameStatus = this.gameStatus;
     return (
       <div>
         <div style={styles.target}>{this.target}</div>
         <div>
-          {this.playNumbers.map((num, i) =>
-            <PlayNumber
-              key={i}
-              id={i}
-              number={num}
-              onClick={this.selectId}
-              isDisabled={
-                gameStatus !== 'PLAYING' || this.state.selectedIds.indexOf(i) >= 0}
-            />
-          )}
+          <div>
+            {this.playNumbers.map((playNumber, index) => (
+              <PlayNumber
+                key={index}
+                number={playNumber}
+                id={index}
+                onClick={this.selectId}
+                isDisabled={
+                  gameStatus !== 'PLAYING' ||
+                  this.state.selectedIds.indexOf(index) >= 0
+                }
+              />
+            ))}
+          </div>
         </div>
-        {this.state.remainingSeconds} <br/>
+        {this.state.remainingSeconds}
+        <br />
         {gameStatus}
+        <br />
+        {this.gameStatus !== 'PLAYING' && (
+          <button onClick={this.props.reset}>Play Again</button>
+        )}
       </div>
     );
   }
