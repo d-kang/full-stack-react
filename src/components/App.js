b0160b81fa7d20a01f5ede54bda23a561e37ff7b
d-kang <david.kang714@gmail.com>
2017-10-05 06:56:47 -0700

    If Disabled Cannot Click Again

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React, { PureComponent } from 'react';
 import Game from './Game';
+// import Quiz from './Quiz';
 
 class App extends PureComponent {
-
   render() {
     return (
-      <Game />
+      <div>
+        {/* {
+          data.map((singleQuestion, i) =>
+            <Quiz singleQuestion={singleQuestion} key={i} />
+          )
+        } */}
+
+        <Game />
+      </div>
     );
   }
 
--- src/components/PlayNumber.js
+++ src/components/PlayNumber.js
@@ -1,13 +1,18 @@
 import React, { PureComponent } from 'react';
 
 class PlayNumber extends PureComponent {
+
   handleClick = () => {
-    console.log('PF', this.props.id, this.props.number)
+    if (this.props.isDisabled) {
+      return;
+    }
+    this.props.onClick(this.props.id);
   }
   render() {
     return (
       <div
-        style={styles.playNumber} className="play-number"
+        style={styles(this.props.isDisabled)}
+        className="play-number"
         onClick={this.handleClick}
       >
         {this.props.number}
@@ -17,17 +22,16 @@ class PlayNumber extends PureComponent {
 
 }
 
-const styles = {
-  playNumber: {
-    border: 'thin solid #c8f7c8',
-    width: '40%',
-    display: 'inline-block',
-    margin: '2.5%',
-    backgroundColor: '#b8ceb8',
-    textAlign: 'center',
-    padding: '0.25rem',
-  }
-};
+const styles = (isDisabled) => ({
+  border: 'thin solid #c8f7c8',
+  width: '40%',
+  display: 'inline-block',
+  margin: '2.5%',
+  backgroundColor: '#b8ceb8',
+  textAlign: 'center',
+  padding: '0.25rem',
+  opacity: isDisabled ? 0.3 : 1
+});
 
 
 
